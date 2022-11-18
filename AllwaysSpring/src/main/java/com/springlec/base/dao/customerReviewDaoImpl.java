463498efc7d19d2661b39bbcc7fc209e07441dfa
hanbyeol <whdlgksquf5@naver.com>
2022-11-18 18:05:25 +0900

    1

--- AllwaysSpring/src/main/java/com/springlec/base/dao/customerReviewDaoImpl.java
+++ AllwaysSpring/src/main/java/com/springlec/base/dao/customerReviewDaoImpl.java
@@ -3,13 +3,11 @@ package com.springlec.base.dao;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.springlec.base.model.customerReviewDto;
 
 public class customerReviewDaoImpl implements customerReviewDao {
 
-	@Autowired
 	SqlSession sqlSession;
 	
 	public static String nameSpace = "com.springlec.base.dao.customerReviewDao";
