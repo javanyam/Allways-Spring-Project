d4d59b121ffc91c06220c8bbed8745acd5e62f67
hanbyeol <whdlgksquf5@naver.com>
2022-11-18 18:05:25 +0900

    1

--- AllwaysSpring/src/main/java/com/springlec/base/controller/customerLoginController.java
+++ AllwaysSpring/src/main/java/com/springlec/base/controller/customerLoginController.java
@@ -1,7 +1,11 @@
 package com.springlec.base.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springlec.base.service.customerLoginDaoService;
 
@@ -11,4 +15,14 @@ public class customerLoginController {
 	@Autowired
 	customerLoginDaoService service;
 	
+	@RequestMapping("/customer/customerLoginPage")
+	public String loginpage() throws Exception{
+		return "customerLogin";
+	}
+	
+	@RequestMapping("/")
+	public String usercheck(HttpServletRequest request, Model model) throws Exception{
+		service.usecheckDao(request.getParameter("customerId"), request.getParameter("customerPw"));
+		return "customerLogin";
+	}
 }
