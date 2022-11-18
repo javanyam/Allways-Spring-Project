eee277c9a63cbc7c39a3d6ac80d2bc5c171b58bb
hanbyeol <whdlgksquf5@naver.com>
2022-11-18 16:50:45 +0900

    1

---
+++ AllwaysSpring/src/main/java/com/springlec/base/service/customerReviewDaoService.java
@@ -0,0 +1,13 @@
+package com.springlec.base.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.springlec.base.model.customerReviewDto;
+
+public interface customerReviewDaoService {
+
+	public List<customerReviewDto> customerReviewList(HttpServletRequest request) throws Exception;
+	
+}
