0adddb58715c463804ebd7e7c3ae6aa2e3d3970e
hanbyeol <whdlgksquf5@naver.com>
2022-11-20 18:58:06 +0900

    JSP폴더 삭제

--- AllwaysSpring/src/main/java/com/springlec/base/dao/customerReviewDao.java
+++ AllwaysSpring/src/main/java/com/springlec/base/dao/customerReviewDao.java
@@ -7,5 +7,6 @@ import com.springlec.base.model.customerReviewDto;
 public interface customerReviewDao {
 
 	public List<customerReviewDto> customerReviewList(String combo, String searchContent, String sort) throws Exception;
+//	public List<customerReviewDto> customerReviewList(String combo, String searchContent, String sort) throws Exception;
 	
 }
