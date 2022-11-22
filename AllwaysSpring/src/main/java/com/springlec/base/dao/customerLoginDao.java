b1a57ba1317df3160ca979bbe973134bfe25584f
hanbyeol <whdlgksquf5@naver.com>
2022-11-22 15:12:04 +0900

    123

--- AllwaysSpring/src/main/java/com/springlec/base/dao/customerLoginDao.java
+++
@@ -1,8 +0,0 @@
-package com.springlec.base.dao;
-
-public interface customerLoginDao {
-
-	public int usercheckDao(String customerId, String customerPw) throws Exception;
-	public String usernameDao(String customerId, String customerPw) throws Exception;
-	
-}
